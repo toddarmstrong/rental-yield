09c455b5612119a00d1ef366ba7417f644a00e50
Todd Armstrong <tenspeedesign@gmail.com>
2015-09-28 10:10:30 +1000

    Added fields and formula to calculate and show rental yield percentage

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var rentalYield = angular.module('rentalYield', ['ionic']);
 
-.run(function($ionicPlatform) {
+rentalYield.run(function($ionicPlatform, $ionicPopup) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -15,5 +15,39 @@ angular.module('starter', ['ionic'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+
+    /*  CALCULATIONS  */
+
+    var buyInput = document.getElementById('buy');
+    var rentInput = document.getElementById('rent');
+    var submit = document.getElementById('submit');
+    var percentageInput = document.getElementById('percentage');
+    var $rentalYield = $('#rental-yield');
+
+    $rentalYield.css("display", "none");
+
+    submit.addEventListener('click', function() {
+      var buy = buyInput.value;
+      var rent = rentInput.value;
+
+      if (!buy || !rent) {
+        $ionicPopup.alert({
+          title: 'Error!',
+          template: 'Please make sure both fields are filled out.'
+        });
+      } else {
+        var rentPerYear = rent * 52;
+
+        var rentalYield = (rentPerYear / buy) * 100;
+
+        percentageInput.innerHTML = rentalYield + '%';
+
+        $rentalYield.fadeTo(500, 1)
+      }
+
+    });
+
+    /*                */
+
   });
-})
+});
